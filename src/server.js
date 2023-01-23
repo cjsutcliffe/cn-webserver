a4da8d2d47d98072deb0e30404fc4c00b587c6d7
Chris Sutcliffe <cjsutcliffe@gmail.com>
2023-01-23 12:16:33 +0000

    initial commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use("/static",express.static("public"));
+// use method says that if anyone types in my webaddress followed /
+// static then it will run express.static with the parameter public. 
+//Express.static sets up a static web server that will send back the webpages in the public folder
+
+app.listen(5001, () => {console.log("listening on Port 5001")});
+//app/listen starts up express and tells it whicj port to listen in on.
+//Also you can run a start up function which in this case just displays the message "listening on Port 5001" 
